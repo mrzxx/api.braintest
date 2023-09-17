bb590bd4ee17635c763d59be68e97b5d5dfd49df
mrzxx <200401056@ogr.comu.edu.tr>
2023-09-17 03:41:50 +0300

    /profile get
    profile get modülü

---
+++ controllers/userdata.js
@@ -0,0 +1,20 @@
+const userdataModel = require('../models/userdata');
+const testdataModel = require('../models/testdata');
+
+exports.get_profile = async (req,res,next) => {
+    try {
+        let data = {...req.user};
+        let arr = Object.keys(data.testresult);
+        let lasttestid = arr[arr.length-1];
+        let gettest = await testdataModel.get_test_by_id(lasttestid);
+        gettest.questions = [];
+        data.testresult = [];
+        gettest.testid = lasttestid;
+        data.testresult = gettest;
+        data.testresult.iq = req.user.testresult[lasttestid].iq;
+        data.testresult.questions = req.user.testresult[lasttestid].questions;
+        res.status(200).json(data);
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/general.js
+++ routes/general.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const categoryController = require('../controllers/category');
 const testdataController = require('../controllers/testdata');
+const userdataController = require('../controllers/userdata');
 // GET /:lang/categories
 router.get('/test',categoryController.test_response);
 router.get('/:lang/categories',categoryController.show_categories_from_lang);
 router.get('/test/:testid', testdataController.show_test_by_id);
 router.get('/answers/:testid', testdataController.check_solved_test_answers);
 router.post('/test/:testid', testdataController.complete_test);
-
+router.get('/profile', userdataController.get_profile);
 module.exports = router;
