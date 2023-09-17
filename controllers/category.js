725837444765449e1fb0c369e6131b4e848363d1
mrzxx <200401056@ogr.comu.edu.tr>
2023-09-17 21:20:19 +0300

    /category fix
    Kategori fixlendi

--- controllers/category.js
+++ controllers/category.js
@@ -6,6 +6,8 @@ exports.test_response = (req,res,next) => {
 }
 
 exports.show_categories_from_lang = async (req,res,next) => {
+
+    /*
     try {
         let get_categories_from_model = await categoryModel.get_categories_from_lang(req.params.lang);
 
@@ -17,6 +19,31 @@ exports.show_categories_from_lang = async (req,res,next) => {
         res.json(get_categories_from_model);
 
 
+    } catch (error) {
+        next(error);
+    }
+
+    */
+
+    try {
+        const data = await categoryModel.get_categories_from_lang(req.params.lang);
+        let returns = [{id:1,title:"Raven Test",tests:[]},{id:2,title:"Logic Test",tests:[]}];    
+        for (let i = 0; i < data.length; i++) {
+            let pick = data[i];
+            pick.questions = null;
+            if(pick.category == 1){
+                Object.keys(req.user.testresult).includes(data[i].testid) ? pick.solved=true : pick.solved=false;
+                Object.keys(req.user.testresult).includes(data[i].requiredtestid) ? pick.locked=false : pick.locked=true;
+                if(data[i].requiredtestid == 0) pick.locked = false;
+                returns[0].tests.push(pick);
+            }else{
+                Object.keys(req.user.testresult).includes(data[i].testid) ? pick.solved=true : pick.solved=false;
+                Object.keys(req.user.testresult).includes(data[i].requiredtestid) ? pick.locked=false : pick.locked=true;
+                if(data[i].requiredtestid == 0) pick.locked = false;
+                returns[1].tests.push(pick);
+            }
+        }
+        res.status(200).json(returns);
     } catch (error) {
         next(error);
     }
